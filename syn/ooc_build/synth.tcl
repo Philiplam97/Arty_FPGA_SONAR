8ba1d17f8ca1b216981765b9aa472fc7bbe13bba
Philip Lam <26860644+Philiplam97@users.noreply.github.com>
2022-03-04 22:09:45 +1100

    Remove generated tcl scripts

--- syn/ooc_build/synth.tcl
+++
@@ -1,5 +0,0 @@
-source ../sonar_prj.tcl
-read_xdc ooc_timing.xdc -mode out_of_context
-synth_design -mode out_of_context -flatten_hierarchy rebuilt -top chirp_gen -part XC7A35TICSG324-1L  -assert
-write_checkpoint -force chirp_gen_build/chirp_gen_synth.dcp
-report_timing_summary -file chirp_gen_build/synth_timing_summary.txt
